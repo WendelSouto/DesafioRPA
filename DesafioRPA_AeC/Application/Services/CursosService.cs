cc94fc83534780c4f4e5170b09b0ca215d9ec5f0
Wendel Souto <souto.wendel@gmail.com>
2023-04-27 15:07:02 -0300

    validation methods elements

--- DesafioRPA_AeC/Application/Services/CursosService.cs
+++ DesafioRPA_AeC/Application/Services/CursosService.cs
@@ -2,12 +2,15 @@
 using DesafioRPA_AeC.Infrastructure.Repositories;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Edge;
+using System.Configuration;
 
 namespace DesafioRPA_AeC.Api.Services
 {
     public interface ICursosService
     {
         void BuscarCursoPorNome(string name);
+        string CheckTeacherElements(List<string> teacherElements);
+        string CheckTimingElements(List<string> timingElements);
     }
 
     public class CursosService:ICursosService
@@ -24,14 +27,17 @@ namespace DesafioRPA_AeC.Api.Services
 
         public void BuscarCursoPorNome(string nome)
         {
-            Console.WriteLine("Buscando pelo curso: "+ nome);
-            Thread.Sleep(1000);
+            var appSettings = ConfigurationManager.AppSettings;
+            var path = "..\\..\\..\\Driver";
+
+            List<string> teacherElements = appSettings["TeacherElements"].Split(",").ToList();
+            List<string> timingElements = appSettings["TimingElements"].Split(",").ToList();
+
             var options = new EdgeOptions
             {
                 PageLoadStrategy = PageLoadStrategy.Normal
             };
 
-            var path = "..\\..\\..\\Driver";
             _driver = new EdgeDriver(path, options);
             _driver.Navigate().GoToUrl($"https://www.alura.com.br/busca?query={nome}");
             _driver.Manage().Window.Maximize();
@@ -44,46 +50,15 @@ namespace DesafioRPA_AeC.Api.Services
             {
                 Curso curso = new();
                 var link = resultado.FindElement(By.ClassName("busca-resultado-link")).GetAttribute("href");
-                curso.Titulo = resultado.FindElement(By.ClassName("busca-resultado-nome")).Text;
-                curso.Descricao = resultado.FindElement(By.ClassName("busca-resultado-descricao")).Text;
+                curso.Titulo = resultado.FindElement(By.ClassName("busca-resultado-nome")).GetAttribute("innerText");
+                curso.Descricao = resultado.FindElement(By.ClassName("busca-resultado-descricao")).GetAttribute("innerText");
                 curso.LinkAcesso = link;
 
                 _driver.SwitchTo().NewWindow(WindowType.Tab);
                 _driver.Navigate().GoToUrl(link);
 
-
-
-
-
-                try
-                {
-                    curso.Professor = _driver.FindElement(By.ClassName("instructor-title--name")).Text;
-                    curso.CargaHoraria = _driver.FindElement(By.ClassName("courseInfo-card-wrapper-infos")).Text;
-                }
-                catch
-                {
-                    try
-                    {
-                        curso.Professor = _driver.FindElement(By.ClassName("formacao-instrutor-nome")).GetAttribute("innerText");
-                        curso.CargaHoraria = _driver.FindElement(By.ClassName("formacao__info-destaque")).Text;
-                    }
-                    catch
-                    {
-                        try
-                        {
-                            curso.Professor = _driver.FindElements(By.TagName("li"))[9].Text;
-                            curso.CargaHoraria = _driver.FindElement(By.ClassName("episode-programming-time")).Text;
-                        }
-                        catch
-                        {
-                            curso.Professor = "Não disponível";
-                            curso.CargaHoraria = "Não disponível";
-                        }
-                    }
-                }
-
-
-
+                curso.Professor = CheckTeacherElements(teacherElements);
+                curso.CargaHoraria = CheckTimingElements(timingElements);
 
                 _driver.Close();
                 _driver.SwitchTo().Window(_driver.WindowHandles.First());
@@ -94,6 +69,27 @@ namespace DesafioRPA_AeC.Api.Services
             _cursoRepository.SalvaCursos(cursos);
         }
 
+        public string CheckTeacherElements(List<string> teacherElements)
+        {
+            foreach (string teacherElement in teacherElements)
+            {
+                if (_driver.FindElements(By.ClassName(teacherElement)).Any())
+                    return _driver.FindElement(By.ClassName(teacherElement)).GetAttribute("innerText");
+            }
+
+            return "Não disponível";
+            
+        }
+
+        public string CheckTimingElements(List<string> timingElements)
+        {
+            foreach (string timingElement in timingElements)
+            {
+                if (_driver.FindElements(By.ClassName(timingElement)).Any())
+                    return _driver.FindElement(By.ClassName(timingElement)).GetAttribute("innerText");
+            }
+            return "Não disponível";
+        }
 
     }
 }
